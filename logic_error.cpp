dad9411e491361d91e9d68fc13f2f7dd57593116
ayangnath <88808124+ayangnath@users.noreply.github.com>
2023-08-04 19:22:16 -0700

    Add files via upload

---
+++ logic_error.cpp
@@ -0,0 +1,37 @@
+//
+//  Code for Project 1
+//  Summer Fun
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string sport;        // the sport being played
+    int won, lost, tied; // game outcomes
+    
+    cout << "What team sport have you been playing this Summer? ";
+    getline( cin, sport );
+    cout << "How many games has your team won? ";
+    cin >> won;
+    cout << "How many games has your team lost? ";
+    cin >> lost;
+    cout << "How many games has your team tied? ";
+    cin >> tied;
+    
+    double winPercentage = 200.0 * won / (won + lost + tied); // Changed the numerator from "100 * won" to "200 * won" to create a logic error
+    
+    cout.setf(ios::fixed);  // see pp. 30-31 in Savitch book
+    cout.precision(1);
+    
+    cout << endl;
+    cout << winPercentage << "% of the " << sport << " games you played were won!" << endl;
+ 
+    if (won < 0 || lost < 0 || tied < 0)
+    {
+        cout << "Looks like some of these data values don't make sense." << endl;
+    }
+
+    return(0);
+}
